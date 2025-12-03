cc1e34e3edbe79bd39553ab8e1b3874e7e004ce0
Driss Guessous <32754868+drisspg@users.noreply.github.com>
2025-12-02 16:24:50 -0800

    Add small exmaple script on use flex flash (#181)

--- attn_gym/__init__.py
+++ attn_gym/__init__.py
@@ -1,6 +1,21 @@
-from attn_gym.utils import visualize_attention_scores
+from attn_gym.utils import (
+    visualize_attention_scores,
+    get_flash_block_size,
+    calculate_tflops,
+    benchmark_cuda_function_in_microseconds,
+    cuda_kernel_profiler,
+)
 import attn_gym.mods
 import attn_gym.masks
 import attn_gym.paged_attention
 
-__all__ = ["visualize_attention_scores", "mods", "masks", "paged_attention"]
+__all__ = [
+    "visualize_attention_scores",
+    "get_flash_block_size",
+    "calculate_tflops",
+    "benchmark_cuda_function_in_microseconds",
+    "cuda_kernel_profiler",
+    "mods",
+    "masks",
+    "paged_attention",
+]
