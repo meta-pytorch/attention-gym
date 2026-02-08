0a9d0dc8f142ed689b6c92cb60b302e815463979
drisspg <drisspguessous@gmail.com>
2026-02-08 18:43:45 +0000

    add activation attention

--- attn_gym/mods/__init__.py
+++ attn_gym/mods/__init__.py
@@ -1,2 +1,3 @@
+from attn_gym.mods.activation import generate_activation_score_mod, undo_softmax
 from attn_gym.mods.alibi import generate_alibi_bias
 from attn_gym.mods.softcapping import generate_tanh_softcap
