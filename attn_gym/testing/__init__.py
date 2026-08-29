f7d5eafd5c81e9c95c7fbd39eff62a386f753192
drisspg <drisspguessous@gmail.com>
2026-08-29 10:45:09 -0700

    Expand KDA context-parallel example

--- attn_gym/testing/__init__.py
+++ attn_gym/testing/__init__.py
@@ -1,5 +1,12 @@
 """Shared test utilities for Attention Gym implementations."""
 
 from .kda import cumulative_sequence_offsets, strided_state_pool
+from .profiling import kernel_stage, profile_trace, record_distributed_profile
 
-__all__ = ["cumulative_sequence_offsets", "strided_state_pool"]
+__all__ = [
+    "cumulative_sequence_offsets",
+    "kernel_stage",
+    "profile_trace",
+    "record_distributed_profile",
+    "strided_state_pool",
+]
